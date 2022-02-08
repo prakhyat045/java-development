781ad61e7e274669112eb87983fcf25cdc50cf4e
Prakhyat Saini <prakhyat.saini@zopsmart.com>
2022-02-08 12:07:43 +0530

    Adding calculator file

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    public static void main(String args[])
+    {
+        System.out.println(add(4,5));
+        System.out.println(subtract(6,3));
+    }
+
+    static int add(int a, int b)
+    {
+        return a+b;
+    }
+
+    static int subtract(int a, int b)
+    {
+        return a - b;
+    }
+}
